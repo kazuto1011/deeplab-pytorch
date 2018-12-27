ffbee6635180fb531c1572104ad06cc855262378
kazuto1011 <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2018-12-27 16:36:04 +0900

    Fix typo

--- libs/models/__init__.py
+++ libs/models/__init__.py
@@ -18,7 +18,7 @@ def init_weights(model):
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             if m.bias is not None:
-                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
 
 def DeepLabV2_ResNet101_MSC(n_classes):
